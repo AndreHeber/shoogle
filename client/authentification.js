beb1573f41e7ef7d0a3358bebf5c8f7a1839d8d4
Andre Heber <andre.heber@gmx.net>
2016-07-16 22:12:36 +0200

    client side javascript rewritten

--- client/authentification.js
+++ client/authentification.js
@@ -1,84 +1,116 @@
-socket.on('login user', function(data) {
-  console.log('data: ' + data);
-  if (data == 'login ok') {
-    vmLogin.status = 'logged in';
-    vmLogin.loginButtonValue = 'Log out';
-    vmLogin.showForm = false;
-  } else if (data == 'unknown user') {
-    vmLogin.status = 'username incorrect';
-    vmLogin.loginButtonValue = 'Login';
-  } else if (data == 'wrong password') {
-    vmLogin.status = 'password incorrect';
-    vmLogin.loginButtonValue = 'Login';
-  } else {}
-});
+var auth = {}
 
-socket.on('register user', function(data) {
-  console.log('data: ' + data);
-  if (data == 'register ok') {
-    vmLogin.status = 'logged in';
-    vmLogin.loginButtonValue = 'Log out';
-    vmLogin.showForm = false;
-  } else if (data == 'username assigned') {
-    vmLogin.status = 'user is already registered';
-    vmLogin.loginButtonValue = 'Login';
-  } else {}
-});
+function initAuth(self) {
+    self.vm = {};
 
-var token = db.getToken(function (err, token) {
-  if ( token ) {
-    socket.emit('login token', token);
-    console.log('transmit token');
-  } else {
-    console.log('no token');
-  }
-});
-
-socket.on('login token', function(data) {
-  if (data == 'login ok') {
-    vmLogin.status = 'logged in';
-    vmLogin.loginButtonValue = 'Log out';
-    vmLogin.showForm = false;
-  }else if (data == 'token invalid') {
-    vmLogin.status = 'logged out';
-    vmLogin.loginButtonValue = 'Login';
-  } else {
-    console.log(data);
-    vmLogin.status = 'wrong token';
-    vmLogin.loginButtonValue = 'Login';
-  }
-});
-
-socket.on('store token', function(data) {
-  db.addToken(data);
-  console.log('store token');
-});
-
-var vmLogin = new Vue({
-  el: '#login',
-  data: {
-    username: '',
-    password: '',
-    status: 'logged out',
-    loginButtonValue: 'Login',
-    showForm: true
-  },
-  methods: {
-    register: function() {
-      socket.emit('register user', {username: this.username, password: this.password});
-    },
-    tryLogin: function() {
-      if (this.loginButtonValue == 'Login') {
-        socket.emit('login user', {username: this.username, password: this.password});
-        this.status = 'try logging in';
-        this.loginButtonValue = 'Please Wait';
-      } else {
-        socket.emit('logout user', {username: this.username});
-        this.status = 'logged out';
-        db.deleteToken();
-        this.loginButtonValue = 'Login';
-        vmLogin.showForm = true;
-      }
+    function loginOk() {
+        self.vm.status = 'logged in';
+        self.vm.loginButtonValue = 'Log out';
+        self.vm.showForm = false;
     }
-  }
+
+    function unknownUser() {
+        self.vm.status = 'username incorrect';
+        self.vm.loginButtonValue = 'Login';
+        self.vm.showForm = true;
+    }
+
+    function wrongPassword() {
+        self.vm.status = 'password incorrect';
+        self.vm.loginButtonValue = 'Login';
+        self.vm.showForm = true;
+    }
+
+    function usernameAssigned() {
+        self.vm.status = 'user is already registered';
+        self.vm.loginButtonValue = 'Login';
+        self.vm.showForm = true;
+    }
+
+    function tokenInvalid() {
+        self.vm.status = 'logged out';
+        self.vm.loginButtonValue = 'Login';
+        self.vm.showForm = true;
+    }
+
+    function wrongToken() {
+        self.vm.status = 'wrong token';
+        self.vm.loginButtonValue = 'Login';
+        self.vm.showForm = true;
+    }
+
+    self.loginUser = function (data) {
+        console.log('data: ' + data);
+        if (data == 'login ok') loginOk();
+        else if (data == 'unknown user') unknownUser();
+        else if (data == 'wrong password') wrongPassword();
+    }
+
+    self.registerUser = function (data) {
+        console.log('data: ' + data);
+        if (data == 'register ok') loginOk();
+        else if (data == 'username assigned') usernameAssigned();
+    }
+
+    self.loginToken = function (data) {
+        if (data == 'login ok') loginOk();
+        else if (data == 'token invalid') tokenInvalid();
+        else wrongToken();
+    }
+
+    self.getToken = function (err, token) {
+        if (token) {
+            socket.emit('login token', token);
+            console.log('transmit token');
+        } else {
+            console.log('no token');
+        }
+    }
+
+    self.storeToken = function (data) {
+        db.addToken(data);
+        console.log('store token');
+    }
+
+    self.login = function () {
+        if (self.vm.loginButtonValue == 'Login') {
+            socket.emit('login user', { username: self.vm.username, password: self.vm.password });
+            self.vm.status = 'try logging in';
+            self.vm.loginButtonValue = 'Please Wait';
+        } else {
+            socket.emit('logout user', { username: self.vm.username });
+            self.vm.status = 'logged out';
+            db.deleteToken();
+            self.vm.loginButtonValue = 'Login';
+            self.vm.showForm = self.vm;
+        }
+    }
+
+    self.register = function () {
+        socket.emit('register user', { username: self.vm.username, password: self.vm.password });
+    }
+ 
+    socket.on('login user', self.loginUser);
+    socket.on('register user', self.registerUser);
+    socket.on('login token', self.loginToken);
+    socket.on('store token', self.storeToken);
+    
+    db.getToken(auth.getToken);
+}
+
+initAuth(auth);
+
+auth.vm = new Vue({
+    el: '#login',
+    data: {
+        username: '',
+        password: '',
+        status: 'logged out',
+        loginButtonValue: 'Login',
+        showForm: true
+    },
+    methods: {
+        register: auth.register,
+        tryLogin: auth.login
+    }
 });
--- client/gmaps.js
+++ client/gmaps.js
@@ -1,36 +1,44 @@
-var map = {
-  instance: {},
-  geolocationPosition: {},
-  markers: [],
-  settings: {
-    center: {lat: 49.446, lng: 11.088},
-    zoom: 16,
-    streetViewControl: false
-  },
-  initMap: function () {
-    this.instance = new google.maps.Map(document.getElementById('map'), this.settings);
-  },
-  addMarker: function () {
-    var marker = new google.maps.Marker({
-      position: map.instance.getCenter(),
-      title: 'Hi!',
-      map: map.instance,
-      draggable: true,
-      animation: google.maps.Animation.DROP,
-      label: 'A'
-    });
-    this.markers.push(marker);
-  },
-  setGeolocationPosition: function() {
-    map.instance.setCenter(this.geolocationPosition);
-  }
-};
+var map = {}
 
-// Get current position from browser (Geolocation) and save it to map
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    map.geolocationPosition = {lat: position.coords.latitude, lng: position.coords.longitude};
-  });
-} else {
-  error('Geo Location is not supported');
+function initMap(self) {
+    self.instance = {};
+    self.geolocationPosition = {};
+    self.markers = [];
+
+    self.settings = {
+        center: { lat: 49.446, lng: 11.088 },
+        zoom: 16,
+        streetViewControl: false
+    }
+
+    self.initMap = function () {
+        self.instance = new google.maps.Map(document.getElementById('map'), self.settings);
+    }
+
+    self.addMarker = function () {
+        var marker = new google.maps.Marker({
+            position: map.instance.getCenter(),
+            title: 'Hi!',
+            map: map.instance,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            label: 'A'
+        });
+        self.markers.push(marker);
+    }
+
+    self.setGeolocationPosition = function() {
+        map.instance.setCenter(self.geolocationPosition);
+    }
+
+    // Get current position from browser (Geolocation) and save it to map
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            self.geolocationPosition = { lat: position.coords.latitude, lng: position.coords.longitude };
+        });
+    } else {
+        error('Geo Location is not supported');
+    }
 }
+
+initMap(map);
